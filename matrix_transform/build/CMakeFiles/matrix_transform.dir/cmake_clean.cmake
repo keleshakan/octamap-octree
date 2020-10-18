30bb685afaaebeb79b6180d5171e76144e358687
HAKAN KELES <hakankeles4152@gmail.com>
2020-10-18 18:34:09 +0300

    Add files via upload

---
+++ matrix_transform/build/CMakeFiles/matrix_transform.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix_transform.dir/matrix_transform.cpp.o"
+  "matrix_transform"
+  "matrix_transform.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrix_transform.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
