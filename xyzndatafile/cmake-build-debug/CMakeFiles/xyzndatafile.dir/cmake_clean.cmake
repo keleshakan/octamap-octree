8da440c2ca33e4eb38f3e7028903421bb0221ae9
HAKAN KELES <hakankeles4152@gmail.com>
2020-10-18 18:36:05 +0300

    Add files via upload

---
+++ xyzndatafile/cmake-build-debug/CMakeFiles/xyzndatafile.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xyzndatafile.dir/main.cpp.o"
+  "xyzndatafile"
+  "xyzndatafile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xyzndatafile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
