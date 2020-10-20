59a0cf60830c97ad3393615bebd32eeb126779fb
HAKAN KELES <hakankeles4152@gmail.com>
2020-10-20 14:59:43 +0300

    Add files via upload

---
+++ simple_octree/cmake-build-debug/CMakeFiles/simple_octree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_octree.dir/main.cpp.o"
+  "simple_octree"
+  "simple_octree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_octree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
